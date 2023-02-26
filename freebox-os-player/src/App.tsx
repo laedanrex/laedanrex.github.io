93e656a45a44597eab4f6282d14d05bc63c8d21f
laedanrex <ldr.paris@gmail.com>
2023-02-26 13:03:23 +0100

    start small react project

---
+++ freebox-os-player/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavigationList from "./navigation/NavigationList";
+
+function App() {
+  return (
+    <div>
+
+      <NavigationList/>
+    </div>
+  )
+    ;
+}
+
+export default App;
---
+++ freebox-os-player/src/navigation/NavigationList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavigationList.css';
+
+function NavigationList() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default NavigationList;
